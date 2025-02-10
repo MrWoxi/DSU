6231afa375193a70f71df8dd4b29a1d99ae18e00
MrWoxi <klimovichvita2005@gmail.com>
2025-02-10 10:57:00 +0300

    base_class

--- lib_dsu/DSU.h
+++ lib_dsu/DSU.h
@@ -1,6 +1,73 @@
-// Copyright 2024 <Victor Klimovich>
+// Copyright 2025 <Victor Klimovich>
 #pragma once
 
 #include <iostream>
 #include <utility>
 #include <algorithm>
+#include <stdexcept>
+
+class DSU {
+    int _size;
+    int* _parent;
+    int* _rank;
+
+public:
+    DSU(int size = 0);
+    ~DSU();
+    void make_set(int elem);
+    int find(int elem);
+    void union_sets(int first, int second);
+    void clear();
+};
+
+DSU::DSU(int size) : _size(size) {
+    _parent = new int[size];
+    _rank = new int[size];
+    for (int i = 0; i < size; ++i) {
+        make_set(i + 1); 
+    }
+}
+
+DSU::~DSU() {
+    delete[] _parent;
+    delete[] _rank;
+}
+
+void DSU::make_set(int elem) {
+    if (elem <= 0 || elem > _size) {
+        throw std::logic_error("Input Error: Element out of bounds.");
+    }
+
+    elem--; 
+
+    _parent[elem] = elem;
+    _rank[elem] = 0;
+}
+
+int DSU::find(int elem) {
+    if (elem <= 0 || elem > _size) {
+        throw std::logic_error("Input Error: Element out of bounds.");
+    }
+
+    elem--; 
+
+    if (_parent[elem] != elem) {
+        _parent[elem] = find(_parent[elem]); 
+    }
+    return _parent[elem];
+}
+
+void DSU::union_sets(int first, int second) {
+    
+    int root1 = find(first);
+    _parent[second] = root1;
+
+}
+
+void DSU::clear() {
+    delete[] _parent;
+    delete[] _rank;
+    _size = 0;
+    _parent = nullptr;
+    _rank = nullptr;
+}
--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -1,13 +1,15 @@
-// Copyright 2025 Klimovich Victor
+// Copyright 2025 <Victor Klimovich>
 
 #include <gtest.h>
 #include "../lib_dsu/DSU.h"
 
+
 #define EPSILON 0.000001
 
+
 TEST(test_true, can_div) {
-	// Arrange
-	int x = 10;
-	int y = 2;
+	DSU d = DSU(10);
+	d.
+
 	ASSERT_TRUE(true);
 }
